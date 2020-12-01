c56d5ecfd9ffcad457ec4f9e1b292d5509166aa7
2dowon <dwon424@gmail.com>
2020-12-02 00:31:12 +0900

    Revert "google seo"
    This reverts commit edc29d8bd8406f92595cb77cd02a18a7bd72d5c2.

--- gatsby-config.js
+++ gatsby-config.js
@@ -93,14 +93,6 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
-    {
-      resolve: 'gatsby-plugin-robots-txt',
-      options: {
-        host: 'https://2dowon.netlify.app',
-        sitemap: 'https://2dowon.netlify.app/sitemap.xml',
-        policy: [{ userAgent: '*', allow: '/' }]
-      }
-    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-feed`,
